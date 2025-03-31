e31e06b93edfd54ffaf778660050a3fef426f556
jieun <j.lim9304@gmail.com>
2025-03-31 13:09:14 +0900

    기타: 패키지 초기화

--- cafe/src/main/java/com/java/cafe/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.java.cafe.repository;
-
-import com.java.cafe.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-}
